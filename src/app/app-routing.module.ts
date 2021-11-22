a233bdc4f3ce622e9c262047d11f00d797310957
iamdeepz <deepakchandrasekaran14@gmail.com>
2021-11-22 21:52:55 +0530

    sale routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: 'inventory', loadChildren: () => import('./components/inventory/inventory.module').then(m => m.InventoryModule)},
   {path: 'customer', loadChildren: () => import('./components/customer/customer/customer.module').then(m => m.CustomerModule)},
+  {path: 'sale', loadChildren: () => import('./components/sale/sale.module').then(m => m.SaleModule)},
   {path: '', redirectTo: 'inventory', pathMatch: 'full'}
 ];
 
